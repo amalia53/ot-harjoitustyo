43b76a9bb8f3018d8c0d4a3e6c6a8038982d29c3
Amalia Ahola <amalia.ahola@gmail.com>
2020-12-01 21:57:11 +0200

    CS

--- OhteSudoku/src/main/java/sudoku/domain/SudokuGame.java
+++ OhteSudoku/src/main/java/sudoku/domain/SudokuGame.java
@@ -15,43 +15,42 @@ public class SudokuGame {
     
     private int[][] solution;
     private int[][] game;
+    private int[][] start;
     
     public SudokuGame() {
         this.solution = new int[9][9];
+        this.start = new int[9][9];
         this.game = new int[9][9];
     }
     
     public int[][] createSolution() {
         //Creates a possible solution
         List<Integer> numbers = new ArrayList<Integer>();
-            for (int row = 0; row < 9; row++) {
-                for (int i = 1; i <=9; i++) {
-                    numbers.add(i);
-                }
-                Collections.shuffle(numbers);
-                for (int col = 0; col < 9; col++) {
-                    int number = getPossibleNumber(row, col, numbers);
-                    if (number == -1) {
-                        solution = new int[9][9];
-                        return null;
-                    } else {
-                        solution[col][row] = number;
-                    }
-                    
+        for (int row = 0; row < 9; row++) {
+            for (int i = 1; i <=9; i++) {
+                numbers.add(i);
+            } Collections.shuffle(numbers);
+            for (int col = 0; col < 9; col++) {
+                int number = getPossibleNumber(row, col, numbers);
+                if (number == -1) {
+                    solution = new int[9][9];
+                    return null;
+                } else {
+                    solution[col][row] = number;
                 }
             }
-        return this.solution;
+        } return this.solution;
     }
     
     public int getPossibleNumber(int row, int col, List<Integer> numbers) {
-            for (int i = 0; i < numbers.size(); i++) {
-                int number = numbers.get(i);
-                boolean column = checkCol(row, col, number);
-                boolean square = checkSquare(row, col, number);
-                if (column && square) {
-                    numbers.remove(i);
-                    return number;
-                }
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+            boolean column = checkCol(row, col, number);
+            boolean square = checkSquare(row, col, number);
+            if (column && square) {
+                numbers.remove(i);
+                return number;
+            }
         } return -1;
     }
     
@@ -84,8 +83,7 @@ public class SudokuGame {
             b = col - 1;
         } if (col % 3 == 0) {
             b = col;
-        }
-        if (row % 3 != 0) {
+        } if (row % 3 != 0) {
             for (int x = a; x < row; x++) {
                 for (int y = b; y < b + 3; y++) {
                     int numberOnField = this.solution[y][x];
@@ -94,26 +92,23 @@ public class SudokuGame {
                     }
                 }
             }
-        }
-        if (col > 0) {
+        } if (col > 0) {
             for (int y = b; y < col; y++) {
                 int numberOnField = this.solution[y][row];
                 if (numberOnField == number) {
                     return false;
                 }
             }
-        } 
-        
-        return true;
+        } return true;
     }
     
     public int[][] createGame() {
         while (createSolution() == null) {
             createSolution();
         }
-        this.game = this.solution;
-        toString(this.game);
-        return this.game;
+        this.start = this.solution;
+        this.game = this.start;
+        return this.start;
     }
     
     public int[][] getCurrentGame() {
@@ -160,13 +155,11 @@ public class SudokuGame {
     }
    
     public void toString(int[][] game) {
-        
         for (int x = 0; x < 9; x++) {
             String rowToString = "";
             for (int y = 0; y < 9; y++) {
                 rowToString = rowToString + game[y][x];
-            }
-            System.out.println(rowToString);
+            } System.out.println(rowToString);
         }
     }
 }
